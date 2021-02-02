6462a4a888c4f074420d8515dd0524fdc2debe26
kikounet85 <kikounet85@gmail.com>
2021-02-02 21:15:04 +0100

    update

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(".calendrier").on('click', function () {
+    window.location.href = "avent/avent.html";
+});
+$(".vérité").on('click', function () {
+    window.location.href = "vérité.html";
+});
+$("div").on("click", function () {
+    $(this).ripples();
+});
