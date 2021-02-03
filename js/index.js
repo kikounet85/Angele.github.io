6bfc5c6611e0e3fe9a75b8bc8d8af7063f897386
kikounet85 <kikounet85@gmail.com>
2021-02-03 02:46:44 +0100

    transition

--- js/index.js
+++ js/index.js
@@ -8,7 +8,11 @@ $("div").on("click", function () {
     $(this).ripples();
 });
 $(document).ready(function() {
-    $("div").animate({top: "-=50"}, 750);
+    setTimeout(function() {
+    $("div").animate({top: "-=50", opacity: "1"}, 750);
+    }, 1200)
+    $("#TB").animate({top: "+=100px", left: "-=100px", opacity: "1"}, 1000);
+    $("#TH").animate({top: "-=100px", left: "+=100px", opacity: "1"}, 1000)
 });
 $("div").hover(function () {
     $(this).animate({top: "+=25"}, 250)
