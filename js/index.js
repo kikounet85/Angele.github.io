788578d1a969d7a46fb15c34a529769682061b46
kikounet85 <kikounet85@gmail.com>
2021-02-04 18:19:11 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -6,17 +6,18 @@ $(".vérité").on('click', function () {
 });
 $(document).ready(function () {
     setTimeout(function () {
-        $(".calendrier, .vérité").animate({ top: "-=50", opacity: "1" }, 750);
-        $("#weather, h1").animate({ opacity: "1"}, 750)
+        $(".calendrier, .vérité").animate({ top: "-=7%", opacity: "1" }, 750);
+        $("#weather, h1").animate({ opacity: "1" }, 750);
+        $("#mode").animate({ opacity: "1", top: "-=5%" }, 750);
     }, 1200)
     $("#TB").animate({ top: "+=8.09%", left: "-=8.09%", opacity: "1", width: "50%" }, 1000);
-    $("#TH").animate({ top: "-=8.09%", left: "+=8.09%", opacity: "1", width: "50%" }, 1000)
+    $("#TH").animate({ top: "-=8.09%", left: "+=8.09%", opacity: "1", width: "50%" }, 1000);
 });
 $(".calendrier, .vérité").hover(function () {
-    $(this).animate({ top: "+=25" }, 250);
+    $(this).animate({ top: "+=6%" }, 250);
 },
     function () {
-        $(this).animate({ top: "-=25" }, 250)
+        $(this).animate({ top: "-=6%" }, 250)
     });
 $.ajax({
     url: "https://geolocation-db.com/jsonp",
@@ -51,7 +52,7 @@ $(document).ready(function () {
             var weather_description;
             var apiKey = "5ac0c007ae8d872392d6a4bbe5fc7080";
 
-            $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longit + "&appid=" + apiKey +"&units=metric", function (data) {
+            $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longit + "&appid=" + apiKey + "&units=metric", function (data) {
 
                 city_name = data["name"];
                 country_name = data["sys"]["country"];
@@ -61,7 +62,7 @@ $(document).ready(function () {
                 wind_speed = data["wind"]["speed"];
 
                 $("#cityname").html(city_name);
-                $(".temp").html(Math.round(temp)+"°");
+                $(".temp").html(Math.round(temp) + "°");
                 $(".pressure").html(pressure + " mBar");
                 $(".wind-spd").html(wind_speed + " m/s");
                 $(".what").html(weather_description);
@@ -78,23 +79,28 @@ var dt = new Date();
 var time = dt.getHours()
 $(document).ready(function () {
     if (time > 18 || time < 10) {
-$("body").addClass("night");
-$(".vérité, .calendrier, #mode").css({"background-color" : "black"});
-$("#mode").addClass("night");
-$("#mode.night").text("Nuit");
-mode++;
+        $("body").addClass("night");
+        $(".vérité, .calendrier, #mode").css({ "background-color": "black" });
+        $("#mode").addClass("night");
+        $("#mode.night").text("Nuit");
+        mode++;
     }
 })
-$("#mode").on('click', function() {
+$("#mode").on('click', function () {
     $("body").toggleClass("night");
     $("#mode").toggleClass("night");
     if (mode == 1) {
-    $(".vérité, .calendrier, #mode").css({"background-color" : "azure"});
-    $("#mode").text("Jour");
-    mode--;
+        $(".vérité, .calendrier, #mode").css({ "background-color": "azure" });
+        $("#mode").text("Jour");
+        mode--;
     } else {
-        $(".vérité, .calendrier, #mode").css({"background-color" : "black"});
+        $(".vérité, .calendrier, #mode").css({ "background-color": "black" });
         $("#mode.night").text("Nuit");
-        mode++ 
+        mode++
     }
+})
+$(document).ready(function () {
+    if ( weather_description == "clear") {
+        $("#TB").attr("src", "img/triangle bas sun.png");
+    };
 })
