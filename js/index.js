2fabe46fa3c27ac605568ff858975ffd95a223cd
kikounet85 <kikounet85@gmail.com>
2021-02-11 21:01:41 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -39,6 +39,10 @@ var dateEN = [
   "Sunday",
 ];
 var minmax0, minmax1, minmax2, minmax3, minmax4;
+function updateWeatherChart(number, content) {
+  chart.data.datasets[number].data = content;
+  chart.update();
+}
 $.ajax({
   url: "https://geolocation-db.com/jsonp",
   jsonpCallback: "callback",
@@ -62,6 +66,7 @@ $(document).ready(function () {
       var plus1;
       var ciel0, ciel1, ciel2, ciel3, ciel4;
       var apiKey = "5ac0c007ae8d872392d6a4bbe5fc7080";
+      var APIn;
       $.getJSON(
         "https://api.openweathermap.org/data/2.5/forecast?lat=" +
           lat +
@@ -74,115 +79,75 @@ $(document).ready(function () {
           if (time == 0 || time >= 22) {
             cntjson = 39;
             plus1 = false;
+            APIn = 0;
           } else if (time >= 1 && time <= 3) {
             cntjson = 39;
             plus1 = true;
+            APIn = 0;
           } else if (time >= 4 && time <= 6) {
             cntjson = 38;
+            APIn = 1;
           } else if (time >= 7 && time <= 9) {
             cntjson = 37;
+            APIn = 2;
           } else if (time >= 10 && time <= 12) {
             cntjson = 36;
+            APIn = 3;
           } else if (time >= 13 && time <= 15) {
             cntjson = 35;
+            APIn = 4;
           } else if (time >= 16 && time <= 18) {
             cntjson = 34;
+            APIn = 5;
           } else if (time >= 19 && time <= 21) {
             cntjson = 33;
+            APIn = 6;
           }
           for (var i = 0; i <= cntjson; i++) {
             weatherlist.push(data["list"][i]["weather"][0]["main"]);
             templist.push(data["list"][i]["main"]["temp"]);
           }
-          if (cntjson == 39 && plus1 == false) {
-            météo0list = templist.slice(0, 9);
-            météo1list = templist.slice(8, 17);
-            météo2list = templist.slice(16, 25);
-            météo3list = templist.slice(24, 33);
-            météo4list = templist.slice(32, 40);
-            ciel0 = weatherlist[4];
-            ciel1 = weatherlist[12];
-            ciel2 = weatherlist[20];
-            ciel3 = weatherlist[28];
-            ciel4 = weatherlist[36];
-          } else if (cntjson == 39 && plus1 == true) {
-            météo0list = templist.slice(0, 8);
-            météo1list = templist.slice(7, 16);
-            météo2list = templist.slice(15, 24);
-            météo3list = templist.slice(23, 32);
-            météo4list = templist.slice(31, 40);
-            ciel0 = weatherlist[3];
-            ciel1 = weatherlist[11];
-            ciel2 = weatherlist[19];
-            ciel3 = weatherlist[27];
-            ciel4 = weatherlist[35];
-          } else if (cntjson == 38) {
-            météo0list = templist.slice(0, 7);
-            météo1list = templist.slice(6, 15);
-            météo2list = templist.slice(14, 23);
-            météo3list = templist.slice(22, 31);
-            météo4list = templist.slice(30, 39);
-            ciel0 = weatherlist[2];
-            ciel1 = weatherlist[10];
-            ciel2 = weatherlist[18];
-            ciel3 = weatherlist[26];
-            ciel4 = weatherlist[34];
-          } else if (cntjson == 37) {
-            météo0list = templist.slice(0, 6);
-            météo1list = templist.slice(5, 14);
-            météo2list = templist.slice(13, 22);
-            météo3list = templist.slice(21, 30);
-            météo4list = templist.slice(29, 38);
-            ciel0 = weatherlist[1];
-            ciel1 = weatherlist[9];
-            ciel2 = weatherlist[17];
-            ciel3 = weatherlist[25];
-            ciel4 = weatherlist[33];
-          } else if (cntjson == 36) {
-            météo0list = templist.slice(0, 5);
-            météo1list = templist.slice(4, 13);
-            météo2list = templist.slice(12, 21);
-            météo3list = templist.slice(20, 29);
-            météo4list = templist.slice(28, 37);
-            ciel0 = weatherlist[0];
-            ciel1 = weatherlist[8];
-            ciel2 = weatherlist[16];
-            ciel3 = weatherlist[24];
-            ciel4 = weatherlist[32];
-          } else if (cntjson == 35) {
-            météo0list = templist.slice(0, 4);
-            météo1list = templist.slice(3, 12);
-            météo2list = templist.slice(11, 20);
-            météo3list = templist.slice(19, 28);
-            météo4list = templist.slice(27, 36);
-            ciel0 = weatherlist[0];
-            ciel1 = weatherlist[7];
-            ciel2 = weatherlist[15];
-            ciel3 = weatherlist[23];
-            ciel4 = weatherlist[31];
-          } else if (cntjson == 34) {
-            météo0list = templist.slice(0, 3);
-            météo1list = templist.slice(2, 11);
-            météo2list = templist.slice(10, 19);
-            météo3list = templist.slice(18, 27);
-            météo4list = templist.slice(26, 35);
-            ciel0 = weatherlist[0];
-            ciel1 = weatherlist[6];
-            ciel2 = weatherlist[14];
-            ciel3 = weatherlist[22];
-            ciel4 = weatherlist[30];
-          } else if (cntjson == 33) {
-            météo0list = templist.slice(0, 2);
-            météo1list = templist.slice(1, 10);
-            météo2list = templist.slice(9, 18);
-            météo3list = templist.slice(17, 26);
-            météo4list = templist.slice(25, 34);
-            ciel0 = weatherlist[0];
-            ciel1 = weatherlist[5];
-            ciel2 = weatherlist[13];
-            ciel3 = weatherlist[21];
-            ciel4 = weatherlist[29];
+          function APImétéo(plus1, i) {
+            if (plus1 == true && i == 0) {
+              météo0list = templist.slice(0, 9);
+              météo1list = templist.slice(8, 17);
+              météo2list = templist.slice(16, 25);
+              météo3list = templist.slice(24, 33);
+              météo4list = templist.slice(32, 40);
+              ciel0 = weatherlist[4];
+              ciel1 = weatherlist[12];
+              ciel2 = weatherlist[20];
+              ciel3 = weatherlist[28];
+              ciel4 = weatherlist[36];
+            } else if (plus1 == false && i == 0) {
+              météo0list = templist.slice(0, 8);
+              météo1list = templist.slice(7, 16);
+              météo2list = templist.slice(15, 24);
+              météo3list = templist.slice(23, 32);
+              météo4list = templist.slice(31, 40);
+              ciel0 = weatherlist[3];
+              ciel1 = weatherlist[11];
+              ciel2 = weatherlist[19];
+              ciel3 = weatherlist[27];
+              ciel4 = weatherlist[35];
+            } else {
+              météo0list = templist.slice(0, 8 - i);
+              météo1list = templist.slice(7 - i, 16 - i);
+              météo2list = templist.slice(15 - i, 24 - i);
+              météo3list = templist.slice(23 - i, 32 - i);
+              météo4list = templist.slice(31 - i, 40 - i);
+              if (i > 3) {
+                ciel0 = weatherlist[0];
+              } else {
+                ciel0 = weatherlist[3 - i];
+              }
+              ciel1 = weatherlist[11 - i];
+              ciel2 = weatherlist[19 - i];
+              ciel3 = weatherlist[27 - i];
+              ciel4 = weatherlist[35 - i];
+            }
           }
+          APImétéo(plus1, APIn);
           ciellist = [$(".what").text(), ciel1, ciel2, ciel3, ciel4];
           météolist = ["#météo0", "#météo1", "#météo2", "#météo3", "#météo4"];
           colorlist = [color0, color1, color2, color3, color4];
@@ -212,7 +177,23 @@ $(document).ready(function () {
           minmax2 = météo2list;
           minmax3 = météo3list;
           minmax4 = météo4list;
-          minmaxlist = [minmax0, minmax1, minmax2, minmax3, minmax4];
+          var Graph0 = $.extend([], météo0list);
+          console.log(Graph0);
+          var Graph1 = $.extend([], météo1list);
+          var Graph2 = $.extend([], météo2list);
+          var Graph3 = $.extend([], météo3list);
+          var Graph4 = $.extend([], météo4list);
+          minmaxlist = [
+            météo0list,
+            météo1list,
+            météo2list,
+            météo3list,
+            météo4list,
+          ];
+          updateWeatherChart(0, Graph1);
+          updateWeatherChart(1, Graph2);
+          updateWeatherChart(2, Graph3);
+          updateWeatherChart(3, Graph4);
         }
       );
     }
@@ -398,7 +379,7 @@ $.fn.nightmode = function () {
       }
     }
     $(
-      ".vérité, .calendrier, body, #mode, #météo1, #météo2, #météo3, #météo4"
+      ".vérité, .calendrier, body, #mode, #météo1, #météo2, #météo3, #météo4, #Graph"
     ).toggleClass("night");
     $("#mode").text("Jour");
     $("#météo0, #météo1, #météo2, #météo3, #météo4").css({ color: "black" });
@@ -406,9 +387,17 @@ $.fn.nightmode = function () {
     $("#TH").attr("src", "img/triangle haut " + weather + ".png");
     $("#carréhaut, #carrébas").attr("src", "img/carré " + weather + ".png");
     $("#cityname").css({ color: "white" });
+    chart.options.legend.labels.fontColor = "black";
+    chart.options.scales.xAxes[0].ticks.fontColor = "black";
+    chart.options.scales.xAxes[0].gridLines.color = "black";
+    chart.options.scales.xAxes[0].scaleLabel.fontColor = "black";
+    chart.options.scales.yAxes[0].ticks.fontColor = "black";
+    chart.options.scales.yAxes[0].gridLines.color = "black";
+    chart.options.scales.yAxes[0].scaleLabel.fontColor = "black";
+    chart.update();
     mode = false;
   } else {
-    $(".vérité, .calendrier, body, #mode").toggleClass("night");
+    $(".vérité, .calendrier, body, #mode, #Graph").toggleClass("night");
     $("#météo0, #météo1, #météo2, #météo3, #météo4").css({
       backgroundColor: "rgb(6, 15, 35)",
       color: "white",
@@ -418,6 +407,14 @@ $.fn.nightmode = function () {
     $("#TH").attr("src", "img/triangle haut night.png");
     $("#carréhaut, #carrébas").attr("src", "img/carré night.png");
     $("#cityname").css({ color: "white" });
+    chart.options.legend.labels.fontColor = "white";
+    chart.options.scales.xAxes[0].ticks.fontColor = "white";
+    chart.options.scales.xAxes[0].gridLines.color = "white";
+    chart.options.scales.xAxes[0].scaleLabel.fontColor = "white";
+    chart.options.scales.yAxes[0].ticks.fontColor = "white";
+    chart.options.scales.yAxes[0].gridLines.color = "white";
+    chart.options.scales.yAxes[0].scaleLabel.fontColor = "white";
+    chart.update();
     mode = true;
   }
   if (mode == true && modemétéo == true) {
@@ -623,3 +620,112 @@ $("#hoverhaut, #weather").on("click", function () {
     trier = true;
   }
 });
+$("#météo0").on("click", function () {
+  showGraph();
+});
+$("#météo1").on("click", function () {
+  showGraph();
+});
+$("#météo2").on("click", function () {
+  showGraph();
+});
+$("#météo3").on("click", function () {
+  showGraph();
+});
+$("#météo4").on("click", function () {
+  showGraph();
+});
+var ctx = document.getElementById("Graphtemp").getContext("2d");
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: ["0h", "3h", "6h", "9h", "12h", "15h", "18h", "21h", "0"],
+    datasets: [
+      {
+        label: "Demain",
+        backgroundColor: "rgb(207, 58, 192)",
+        borderColor: "rgb(207, 58, 192)",
+        fill: false,
+      },
+      {
+        label: "Demain + 1",
+        backgroundColor: "rgb(136, 5, 250)",
+        borderColor: "rgb(136, 5, 250)",
+        fill: false,
+      },
+      {
+        label: "Demain + 2",
+        backgroundColor: "rgb(132, 250, 5)",
+        borderColor: "rgb(132, 250, 5)",
+        fill: false,
+      },
+      {
+        label: "Demain + 3",
+        backgroundColor: "rgb(250, 152, 5)",
+        borderColor: "rgb(250, 152, 5)",
+        fill: false,
+      },
+    ],
+  },
+
+  options: {
+    legend: {
+      labels: {
+        fontColor: "black",
+        fontSize: 20,
+      },
+    },
+    responsive: true,
+    scales: {
+      xAxes: [
+        {
+          display: true,
+          ticks: {
+            fontColor: "black",
+            fontSize: 20,
+          },
+          gridLines: {
+            display: true,
+            color: "black",
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Heure",
+            fontColor: "black",
+            fontSize: 20,
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false,
+            fontColor: "black",
+            fontSize: 20,
+          },
+          display: true,
+          gridLines: {
+            display: true,
+            color: "black",
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Température (°)",
+            fontColor: "black",
+            fontSize: 20,
+          },
+        },
+      ],
+    },
+  },
+});
+function showGraph(i) {
+  $("#météo0").animate({ top: "-=40%", opacity: "0" }, 750);
+  $("#météo2, #météo3").animate({ top: "+=40%", opacity: "0" }, 750);
+  $("#météo1").animate({ left: "-=30%", opacity: "0" }, 750);
+  $("#météo4").animate({ left: "+=30%", opacity: "0" }, 750);
+  $("#Graph").animate({ top: "30%", opacity: "1" }, 1000);
+}
