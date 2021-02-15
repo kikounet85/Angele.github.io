ec2028913ee379dca66b33dcbefc27f1a6a9e75b
kikounet85 <kikounet85@gmail.com>
2021-02-15 21:35:48 +0100

    Update

--- js/vérité.js
+++ js/vérité.js
@@ -1,7 +1,46 @@
 var mydata = JSON.parse(data);
 var length = mydata.length;
+var countbutton = 0;
+var result = 0;
+var dateFR = [
+  "lundi",
+  "mardi",
+  "mercredi",
+  "jeudi",
+  "vendredi",
+  "samedi",
+  "dimanche",
+];
+var dateEN = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+var dateact = moment().format("dddd");
+for (i = 0; i <= 6; i++) {
+  if (dateact == dateEN[i]) {
+    dateact = dateFR[i];
+  }
+}
 console.log(length);
-var random = Math.floor(Math.random() * Math.floor(length));
-var LOL = mydata[random].replace("_", "'");
-var LOL = mydata[random].replace("<", moment().format("dddd"));
-console.log(LOL);
+$("#countmax").text("Il y a " + length + " phrases pour toi :)");
+$("#bouton").on("click", function () {
+  var random = Math.floor(Math.random() * Math.floor(length));
+  var LOL = mydata[random].replace("<", dateact);
+  alert(LOL);
+  countbutton++;
+  result = (countbutton / length) * 100;
+  if (result >= 100) {
+    $("#probability").text(
+      "Tu as 100% de chance de tomber sur une phrase déjà vu !"
+    );
+  } else {
+    $("#probability").text(
+      "Tu as " + result + "% de chance de tomber sur une phrase déjà vu !"
+    );
+  }
+});
