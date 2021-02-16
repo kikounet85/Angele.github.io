71b4200bd891ae2b51cca5c93fc77e10361318ad
kikounet85 <kikounet85@gmail.com>
2021-02-16 01:53:49 +0100

    Update vérité 2

--- js/vérité.js
+++ js/vérité.js
@@ -2,6 +2,12 @@ var mydata = JSON.parse(data);
 var length = mydata.length;
 var countbutton = 0;
 var result = 0;
+var action5 = 0;
+var day1 = new Date("10/02/2020");
+var day2 = new Date(moment().format("l"));
+var difference = Math.abs(day2 - day1);
+var random1, random2, random3;
+var begin = false;
 var dateFR = [
   "lundi",
   "mardi",
@@ -26,21 +32,47 @@ for (i = 0; i <= 6; i++) {
     dateact = dateFR[i];
   }
 }
-console.log(length);
+loveday = difference / (1000 * 3600 * 24);
+loveday = Math.round(loveday);
 $("#countmax").text("Il y a " + length + " phrases pour toi :)");
-$("#bouton").on("click", function () {
+$("#mapBouton").on("click", function () {
   var random = Math.floor(Math.random() * Math.floor(length));
-  var LOL = mydata[random].replace("<", dateact);
-  alert(LOL);
+  LOL = mydata[random];
+  if (LOL.includes("%actualday")) {
+    LOL = mydata[random].replace("%actualday", dateact);
+  } else if (LOL.includes("%loveday")) {
+    LOL = mydata[random].replace("%loveday", loveday);
+  }
+  if (random == 5 && action5 == 0) {
+    $("#bouton").animate({ top: "75%", left: "75%" }, 500);
+    action5++;
+  } else if (action5 == 1) {
+    $("#bouton").animate({ left: "15%" }, 500);
+    action5++;
+  } else if (action5 == 2) {
+    $("#bouton").animate({ left: "43%", top: "30%" }, 500);
+    action5 = 0;
+  } else alert(LOL);
   countbutton++;
   result = (countbutton / length) * 100;
+  console.log(action5);
   if (result >= 100) {
     $("#probability").text(
       "Tu as 100% de chance de tomber sur une phrase déjà vu !"
     );
   } else {
     $("#probability").text(
-      "Tu as " + result + "% de chance de tomber sur une phrase déjà vu !"
+      "Tu as " +
+        result +
+        "% de chance de tomber sur une phrase que tu as déjà vu !"
     );
   }
 });
+$("#mapBouton").hover(
+  function () {
+    $("#bouton").css({ transform: "scale(1.15)" });
+  },
+  function () {
+    $("#bouton").css({ transform: "scale(1)" });
+  }
+);
