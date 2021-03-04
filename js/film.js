aeabf1346fda147ece03316ec4ff28c8a9ae8670
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2021-03-04 20:57:36 +0100

    Film1

--- js/film.js
+++ js/film.js
@@ -1,23 +1,55 @@
+var TrentejoursMax = "/movie/613377";
+var LeVoyageDeChihiro = "/movie/129";
+var LeTombeauDesLucioles = "/movie/12477";
+var YourName = "/movie/372058";
+var Jojo = "/tv/45790";
+var Nausicaa = "/movie/81";
+var Pokemon1b = "/movie/571891";
+var TheOffice = "/tv/2316";
 var Film = [
-  "/tv/2316?api_key=c8ba3cbfd981404e3c6a588adfbce2d5&language=fr",
-  "/movie/372058?api_key=c8ba3cbfd981404e3c6a588adfbce2d5&language=fr-FR",
-  "/movie/613377?api_key=c8ba3cbfd981404e3c6a588adfbce2d5&language=fr-FR",
+  TrentejoursMax,
+  LeVoyageDeChihiro,
+  LeTombeauDesLucioles,
+  YourName,
+  Jojo,
+  Nausicaa,
+  Pokemon1b,
+  TheOffice,
 ];
 
 $(document).ready(function () {
   for (let i = 0; i < Film.length; i++) {
-    console.log("before " + i);
-    $.getJSON("https://api.themoviedb.org/3" + Film[i], function (data) {
-      console.log(data);
-      console.log(i);
-      $("#Title" + i).text(data["name"]);
-      $("#Title" + i).text(data["title"]);
-      $("#Summary" + i).text(data["overview"]);
-      $("#Genre" + i).text(data["genres"][0]["name"]);
-      $("#Poster" + i).attr(
-        "src",
-        "https://image.tmdb.org/t/p/w500" + data["poster_path"]
-      );
-    });
+    $.getJSON(
+      "https://api.themoviedb.org/3" +
+        Film[i] +
+        "?api_key=c8ba3cbfd981404e3c6a588adfbce2d5&language=fr",
+      function (data) {
+        var ID = "Film" + i;
+        console.log(ID), $("body").append('<div id="' + ID + '"></div>');
+        let Title = data["title"];
+        if (Title === undefined) {
+          $("#" + ID).append(
+            '<h2 id="Title' + i + '">' + data["name"] + "</h2>"
+          );
+        } else {
+          $("#" + ID).append(
+            '<h2 id="Title' + i + '">' + data["title"] + "</h2>"
+          );
+        }
+        $("#" + ID).append(
+          '<h4 id="Genre' + i + '">' + data["genres"][0]["name"] + "</h4>"
+        );
+        $("#" + ID).append(
+          '<h4 id="Summary' + i + '">' + data["overview"] + "</h4>"
+        );
+        $("#" + ID).append(
+          '<img id="Summary' +
+            i +
+            '" src="https://image.tmdb.org/t/p/w500' +
+            data["poster_path"] +
+            '"/>'
+        );
+      }
+    );
   }
 });
