ddb6ee5d063cf382111fda422b71c3c322f922f9
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2021-03-05 10:02:41 +0100

    Update film.js

--- js/film.js
+++ js/film.js
@@ -16,46 +16,48 @@ var Film = [
   Pokemon1b,
   TheOffice,
 ];
-var FilmNumber = 0;
+function ActualFilm(FilmNumber) {
+  $.getJSON(
+    "https://api.themoviedb.org/3" +
+      Film[FilmNumber] +
+      "?api_key=c8ba3cbfd981404e3c6a588adfbce2d5&language=fr",
+    function (data) {
+      var ID = "Film" + FilmNumber;
+      console.log(ID), $("body").append('<div id="' + ID + '"></div>');
+      let Title = data["title"];
+      if (Title === undefined) {
+        $("#" + ID).append(
+          '<h2 id="Title' + FilmNumber + '">' + data["name"] + "</h2>"
+        );
+      } else {
+        $("#" + ID).append(
+          '<h2 id="Title' + FilmNumber + '">' + data["title"] + "</h2>"
+        );
+      }
+      $("#" + ID).append(
+        '<h4 id="Genre' +
+          FilmNumber +
+          '">' +
+          data["genres"][0]["name"] +
+          "</h4>"
+      );
+      $("#" + ID).append(
+        '<h4 id="Summary' + FilmNumber + '">' + data["overview"] + "</h4>"
+      );
+      $("#" + ID).append(
+        '<img id="Poster' +
+          FilmNumber +
+          '" src="https://image.tmdb.org/t/p/w500' +
+          data["poster_path"] +
+          '"/>'
+      );
+      setTimeout(50);
+    }
+  );
+}
 
 $(document).ready(function () {
-  $(Film).each(function () {
-    $.getJSON(
-      "https://api.themoviedb.org/3" +
-        this +
-        "?api_key=c8ba3cbfd981404e3c6a588adfbce2d5&language=fr",
-      function (data) {
-        var ID = "Film" + FilmNumber;
-        console.log(ID), $("body").append('<div id="' + ID + '"></div>');
-        let Title = data["title"];
-        if (Title === undefined) {
-          $("#" + ID).append(
-            '<h2 id="Title' + FilmNumber + '">' + data["name"] + "</h2>"
-          );
-        } else {
-          $("#" + ID).append(
-            '<h2 id="Title' + FilmNumber + '">' + data["title"] + "</h2>"
-          );
-        }
-        $("#" + ID).append(
-          '<h4 id="Genre' +
-            FilmNumber +
-            '">' +
-            data["genres"][0]["name"] +
-            "</h4>"
-        );
-        $("#" + ID).append(
-          '<h4 id="Summary' + FilmNumber + '">' + data["overview"] + "</h4>"
-        );
-        $("#" + ID).append(
-          '<img id="Poster' +
-            FilmNumber +
-            '" src="https://image.tmdb.org/t/p/w500' +
-            data["poster_path"] +
-            '"/>'
-        );
-        FilmNumber++;
-      }
-    );
-  });
+  for (let i = 0; i < Film.length; i++) {
+    ActualFilm(i);
+  }
 });
